1aee1a01cf632b50f122ec83ab105cd430473899
Thành Trung <npttrung2711@gmail.com>
2024-10-25 13:39:15 +0700

    + textfield delegate protocol

--- UIKitBootcamp/Basics/TextFields/TextFieldViewController.swift
+++ UIKitBootcamp/Basics/TextFields/TextFieldViewController.swift
@@ -21,12 +21,31 @@ class TextFieldViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.delegate = self
+        textField.addTarget(self, action: #selector(onEdditingChanged), for: .editingChanged)
         view.addSubview(textField)
         view.backgroundColor = .white
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         textField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         textField.widthAnchor.constraint(equalToConstant: view.frame.width - 64).isActive = true
+    }
+    @objc func onEdditingChanged(_ sender: UITextField) {
+        print("DEBUG: Text field is \(sender.text)")
+    }
+}
+
+extension TextFieldViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        print("DEBUG: Return key pressed")
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("DEBUG: Text field is editing")
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("DEBUG: Text field is not editing")
     }
 }
 
